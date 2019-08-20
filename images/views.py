078559a113a85857d84088ce00b32f1944d95ca4
Artur <artwis21@gmail.com>
2019-08-20 09:24:53 +0200

    Add actions

--- images/views.py
+++ images/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm
 from .models import Image
 from common.decorators import ajax_required
+from actions.utils import create_action
 
 @login_required
 def image_create(request):
@@ -19,6 +20,7 @@ def image_create(request):
             new_item = form.save(commit=False)
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'dodał obraz', new_item)
             messages.success(request, 'Obraz został dodany.')
             #Przekierowanie do widoku nowo utworzonego elementu.
             return redirect(new_item.get_absolute_url())
@@ -52,6 +54,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'polubił', image)
             else:
                 image.user_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
